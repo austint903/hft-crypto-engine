72f888d1fad6bc37349cb95ded689c6df93d58e4
austint-code <austintan2006@gmail.com>
2025-07-27 23:14:49 -0400

    set up separate websocket for orders

--- include/OrderManager.hpp
+++ include/OrderManager.hpp
@@ -1,15 +1,31 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <nlohmann/json.hpp> 
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
 #include <string>
 #include <mutex>
 #include <atomic>
 #include <unordered_map>
 #include <functional>
+#include <algorithm>
+#include <cctype>
+#include <thread>
+
 
 
 enum class OrderSide{BUY, SELL};
 enum class OrderStatus { NEW, ACKED, PARTIAL, FILLED, CANCELED, REJECTED };
+
+namespace ssl = boost::asio::ssl;
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace ws = beast::websocket;
+using tcp = asio::ip::tcp;
+using json = nlohmann::json;
 
 struct Order{
     std::string clientId;
@@ -27,7 +43,7 @@ class OrderManager{
     //type aliasing for any callable that takes in order
     using OrderCallback = std::function<void(const Order&)>;
     public:
-        explicit OrderManager(boost::asio::io_context& ioc);
+        explicit OrderManager(asio::io_context& ioc,ssl::context& sll_ctx, std::string& host, std::string& port);
         ~OrderManager();
         //returns client Id
         std::string sendOrder(OrderSide side, double quantity, double price, const std::string& symbol);
@@ -38,14 +54,20 @@ class OrderManager{
         
         void cancelOrder(const std::string& clientId);
         void handleExchangeAcknowledge(const std::string& clientId, double filledQuantity, double filledPrice, bool success);
+        void run();
+        void stop();
 
 
 
     private:    
+
+
+
         void doSend(const std::string& payload);
-        boost::asio::io_context& ioc;
+        asio::io_context& ioc;
+
         //help with concurrency (in order)/ thread safety
-        boost::asio::strand<boost::asio::io_context::executor_type> strand;
+        asio::strand<boost::asio::io_context::executor_type> strand;
 
         std::mutex mu;  
         //eliminates race conditions, unique id
@@ -56,4 +78,20 @@ class OrderManager{
 
         //registered callback
         OrderCallback callback;
+
+        //websocket
+        void doResolve();
+        void onResolve(beast::error_code ec, tcp::resolver::results_type results);
+        void onConnect(beast::error_code ec, tcp::endpoint);
+        void onSslHandShake(beast::error_code ec);
+        void onWsHandshake(beast::error_code ec);
+        void onRead(beast::error_code ec, std::size_t bytes_transferred);
+        ssl::context& ssl_ctx;
+        tcp::resolver resolver;
+        ws::stream<ssl::stream<tcp::socket>> ws;
+        beast::flat_buffer buffer;
+        std::string port;
+        std::string host;
+        std::thread thread;
+        std::atomic<bool> running{false};
 };
--- src/OrderManager.cpp
+++ src/OrderManager.cpp
@@ -1,10 +1,95 @@
 #include <OrderManager.hpp>
-#include <nlohmann/json.hpp>
 #include <iostream>
+#include <exception>
 
-OrderManager::OrderManager(boost::asio::io_context& ioc):ioc(ioc), strand(ioc.get_executor()){}
+OrderManager::OrderManager(asio::io_context& ioc, ssl::context& ssl_ctx,  std::string& host, std::string& port):ioc(ioc), ssl_ctx(ssl_ctx), resolver(asio::make_strand(ioc)), ws(asio::make_strand(ioc), ssl_ctx), strand(ioc.get_executor()) ,host(std::move(host)), port(std::move(port)){}
 
 OrderManager::~OrderManager() = default;
+
+void OrderManager::run() {
+    if(running)return;
+    running=true;
+    thread=std::thread([this]{
+        this->doResolve();
+        ioc.run();
+    });
+}
+
+void OrderManager::stop(){
+    if(!running)return;
+    running=false;
+    beast::error_code ec;
+    ws.close(beast::websocket::close_code::normal, ec);
+    //stop sll
+    ws.next_layer().shutdown(ec);
+    ws.next_layer().next_layer().shutdown(tcp::socket::shutdown_both,ec);
+    ioc.stop();
+    if(thread.joinable()){
+        thread.join();
+    }
+}
+void OrderManager::doResolve(){
+    resolver.async_resolve(
+        host, port, beast::bind_front_handler(&OrderManager::onResolve, this)
+    );
+}
+
+void OrderManager::onResolve(beast::error_code ec, tcp::resolver::results_type results){
+    if(ec){
+        std::cerr<<"resolve error "<<ec.message()<<"\n";
+        return;
+    }
+    //start Tcp conneciton
+    asio::async_connect(
+        ws.next_layer().next_layer(), results, beast::bind_front_handler(&OrderManager::onConnect, this)
+    );
+}
+
+void OrderManager::onConnect(beast::error_code ec, tcp::endpoint){
+    if (ec) {
+        std::cerr << "Connect error: " << ec.message() << "\n";
+        return;
+    }
+    //TLS handshake
+    ws.next_layer().async_handshake(
+        ssl::stream_base::client, beast::bind_front_handler(&OrderManager::onSslHandShake, this)
+    );
+
+}
+
+void OrderManager::onSslHandShake(beast::error_code ec){
+    if(ec){
+        std::cerr<<"ssl handshake error "<<ec.message()<<"\n";
+        return;
+    }
+    const std::string target ="/ws";
+    const std::string hostHeader = host + ":" +port;
+    ws.async_handshake(hostHeader, target,beast::bind_front_handler(&OrderManager::onWsHandshake, this) );
+}
+
+void OrderManager::onWsHandshake(beast::error_code ec){
+    if(ec){
+        std::cerr<<"ws handshake error "<<ec.message()<<"\n";
+        return;
+    }
+    ws.async_read(buffer, beast::bind_front_handler(&OrderManager::onRead, this));
+}
+
+void OrderManager::onRead(beast::error_code ec, std::size_t){
+    if(ec){
+        std::cerr<<"read error "<<ec.message()<<"\n";
+        return;
+    }
+    if(!running){
+        return;
+    }
+    try{
+        std::cout<<"Running"<<"\n";
+    }catch (const std::exception& e){
+        std::cerr<<"error "<<e.what()<<"\n";
+    }
+}
+
 
 std::string OrderManager::sendOrder(OrderSide side, double quantity, double price, const std::string& symbol){
     std::string id = "client " + std::to_string(nextId.fetch_add(1));
@@ -52,6 +137,8 @@ void OrderManager::cancelOrder(const std::string& clientId){
 void OrderManager::onOrderUpdate(OrderCallback cb){
     callback = std::move(cb);
 }
+
+//consider removing each order from map once filled or rejected
 void OrderManager::handleExchangeAcknowledge(const std::string& clientId, double filledQuantity, double filledPrice, bool success){
     Order updated;
     {
@@ -89,6 +176,8 @@ void OrderManager::handleExchangeAcknowledge(const std::string& clientId, double
         callback(updated);
     }
 }   
-void OrderManager::doSend(const std::string& payload){
 
+//add error handling for bind_executor
+void OrderManager::doSend(const std::string& payload){
+    ws.async_write(asio::buffer(payload), asio::bind_executor(strand));
 }
