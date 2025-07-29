39917e3eebe1e748cfc9ba346b4d805675037873
austint-code <austintan2006@gmail.com>
2025-07-29 18:07:33 -0400

    rolling stats hpp

---
+++ include/RollingStats.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <deque>
+#include <cstddef>
+#include <cmath>
+
+class RollingStats {
+    private:
+        std::deque<double> buf;
+        size_t window;
+        double sum;
+        double sumSq;
+        
+    public:
+        explicit RollingStats(size_t window);
+        void add(double x);
+        bool ready() const;
+        double mean() const;
+        double stddev() const;
+        size_t size() const;
+        void clear();
+    };
